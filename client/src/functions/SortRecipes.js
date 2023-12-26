c015f1a37a558004e1042cf469c78dcfc51a4e3f
davfu <daveyblazefoo@gmail.com>
2023-12-25 18:46:27 -0800

    init for sort recipes

---
+++ client/src/functions/SortRecipes.js
@@ -0,0 +1,8 @@
+// after API gets the recipe list, sort the recipes based on user preference
+import React, { useState, useEffect } from 'react';
+
+const SortRecipes = () => {
+    
+}
+
+export default SortRecipes;
