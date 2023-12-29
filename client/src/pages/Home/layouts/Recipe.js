8fc964d7421e336f7cec38e7e903718a259278df
davfu <daveyblazefoo@gmail.com>
2023-12-28 18:47:17 -0800

    added more parameters to display

--- client/src/pages/Home/layouts/Recipe.js
+++ client/src/pages/Home/layouts/Recipe.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Recipe({ recipe }) {
   return (
     <div>
-        <li><a href={recipe.url}>{recipe.name}</a></li>
+        <li><a href={recipe.url}>{recipe.name}</a>, {recipe.num_rev} reviews, {recipe.protein} g Protein, {recipe.cals} Calories, {recipe.num_ing} Ingredients.</li>
     </div>
   )
 }
