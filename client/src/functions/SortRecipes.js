2708768ad0f0af79ba01732251a6108e80f2d6c7
davfu <daveyblazefoo@gmail.com>
2023-12-28 20:04:55 -0800

    rmv SortRecipes.js

--- client/src/functions/SortRecipes.js
+++
@@ -1,8 +0,0 @@
-// after API gets the recipe list, sort the recipes based on user preference
-import React, { useState, useEffect } from 'react';
-
-const SortRecipes = () => {
-    
-}
-
-export default SortRecipes;
